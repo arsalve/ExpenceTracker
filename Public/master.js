a5767ab3dfab0a24a333445a46554f1087243e39
Ashish Salve <a.r.salve@gmail.com~>
2023-10-15 03:11:59 +0530

    JS with Visualization (pie chart Added)

--- Public/master.js
+++ Public/master.js
@@ -1,15 +1,16 @@
 // Define dropdown values
 var dropdownValues = {
 
-    "Savings": ["म्युच्युअल फंड्स", "एफडी", "आरडी", "सोने", "विमा", "पीपीएफ","इतर गुंतवणूक"],
+    "Savings": ["म्युच्युअल फंड्स", "एफडी", "आरडी", "सोने", "विमा", "पीपीएफ", "इतर गुंतवणूक"],
     "Income": ["पगार", "गुंतवणूकीचा परतावा", "इतर उत्पन्न"],
-    "Expence": ["आहार", "किराणा","घरातील खर्च", "परिवहन", "मनोरंजन", "दूरसंचार","आरोग्य","वैयक्तिक काळजी", "कपडे", "वाहन देखरेख", "इंधन", "रोकड", "वॉलेट ट्रान्स्फर","इतर खर्च"]
+    "Expence": ["आहार", "किराणा", "घरातील खर्च", "परिवहन", "मनोरंजन", "दूरसंचार", "आरोग्य", "वैयक्तिक काळजी", "कपडे", "वाहन देखरेख", "इंधन", "रोकड", "वॉलेट ट्रान्स्फर", "इतर खर्च"]
 };
 // Get references to the form and table elements
 var form = document.querySelector('form');
 var table = document.querySelector('table');
 const tableog = document.getElementById("entries").innerHTML;
 const summuryog = document.getElementById("summury").innerHTML;
+const catelog = document.getElementById("cate").innerHTML;
 // Get references to the two dropdown lists
 const firstDropdown = document.getElementById('ExpenseType');
 const secondDropdown = document.getElementById('ExpenseOption');
@@ -49,12 +50,115 @@ function updateSecondDropdown() {
         secondDropdown.appendChild(optionElement);
     }
 }
+
+function sumArray(arr) {
+    let sum = 0;
+    // Calculation the sum using forEach
+    arr.forEach(x => {
+        sum += Number(x);
+    });
+    return sum;
+}
+
+function Ploy(xAxis, yAxis, parents) {
+    // var labels=((["खर्च","बचत","उत्पन्न"].concat(xAxis[0])).concat(xAxis[1])).concat(xAxis[2])
+    // var sum1=Math.abs((-sumArray(yAxis[0]))-sumArray(yAxis[1])+sumArray(yAxis[2]));
+    // var Debit1=sumArray(yAxis[0]);
+    // var Saving1=sumArray(yAxis[1]);
+    // var Income1=sumArray(yAxis[2]);
+    // var value=(([Debit1,Saving1,Income1]).concat(yAxis[0])).concat(yAxis[1])).concat(yAxis[2]);
+    // var parent=((["",""].concat(parents[0])).concat(parents[1])).concat(parents[2]);
+    // var dataS = [{
+    //     type: "sunburst",
+    //     labels: labels,
+    //     parents: parent,
+    //     values:  value,
+    //     outsidetextfont: {size: 20, color: "#377eb8"},
+    //     leaf: {opacity: 0.4},
+    //     marker: {line: {width: 2}},
+    //   }];
+
+    //   var layoutS = {
+    //     margin: {l: 0, r: 0, b: 0, t: 0},
+    //     width: 500,
+    //     height: 500
+    //   };
+
+
+    //   Plotly.newPlot('sunburst', dataS, layoutS);
+
+
+    //
+
+    var data = [{
+        type: "pie",
+        values: yAxis[0],
+        labels: xAxis[0],
+        textinfo: "label+percent",
+        textposition: "inside",
+
+        automargin: true,
+        size: 500,
+        hole: .4,
+        text: 'खर्च'
+
+    }];
+    var data2 = [{
+        type: "pie",
+        values: yAxis[1],
+        labels: xAxis[1],
+        textinfo: "label+percent",
+
+        textposition: "inside",
+        automargin: true,
+        hole: .4,
+        size: 500,
+        text: 'बचत'
+
+    }];
+    var data3 = [{
+        type: "pie",
+        values: yAxis[2],
+        labels: xAxis[2],
+        textinfo: "label+percent",
+        textposition: "inside",
+        automargin: true,
+        text: 'उत्पन्न',
+        hole: .4,
+        size: 500
+
+    }]
+
+    var layout = {
+        margin: {
+            autoexpand: true
+        },
+        autosize: false,
+        height: 400,
+        width: 500,
+        grid: {
+            rows: 1,
+            columns: 1
+        }
+    };
+
+
+
+    Plotly.newPlot('Exp', data, layout);
+    Plotly.newPlot('Save', data2, layout);
+    Plotly.newPlot('Income', data3, layout);
+
+
+}
+
 // Get the table element from HTML
 function displayData() {
     document.getElementById("entries").innerHTML = tableog;
     document.getElementById("summury").innerHTML = summuryog;
+    document.getElementById("cate").innerHTML = catelog;
     var table = document.getElementById("entries");
     var summury = document.getElementById("summury");
+    var cate = document.getElementById("cate");
     var today = new Date()
     if (document.querySelector("#selectMonth").value == '')
         document.querySelector("#selectMonth").value = today.getFullYear() + "-" + today.toLocaleString('default', {
@@ -81,12 +185,41 @@ function displayData() {
             var savings = 0;
             var expence = 0;
             var income = 0;
+            var xAxis = [
+                [],
+                [],
+                []
+            ];
+            var yAxis = [
+                [],
+                [],
+                []
+            ];
+            var parent = [
+                [],
+                [],
+                []
+            ];
+            var counters = {
+                Savings: {},
+                Income: {},
+                credit: {}
+            };
             res.json().then((data) => {
                 // Loop through the data
                 var TX = data;
                 TX.forEach((Entry) => {
                     // Get the month from the date
                     var item = Entry.transaction;
+                    var element = item.description;
+                    var type = item.type;
+                    if (!counters[type]) {
+                        counters[type] = {};
+                        counters[type][element] = 0;
+                        counters[type][element] = Number(counters[type][element] ? counters[type][element] + Number(item.amount) : Number(item.amount));
+                    } else {
+                        counters[type][element] = Number(counters[type][element] ? counters[type][element] + Number(item.amount) : Number(item.amount));
+                    }
                     // Create a new table row
                     var row = document.createElement("tr");
                     var cell1 = document.createElement("td");
@@ -105,15 +238,18 @@ function displayData() {
                     switch (item.type) {
                         case "credit":
                             cell3.innerHTML = "उत्पन्न";
+                            parent[2].push("उत्पन्न");
                             cell3.style.backgroundColor = "#12b1127d";
                             break;
                         case "debit":
                             cell3.innerHTML = "खर्च";
                             cell3.style.backgroundColor = "#b119127d";
+                            parent[0].push("खर्च");
                             break;
                         case "Savings":
                             cell3.innerHTML = "बचत";
                             cell3.style.backgroundColor = "#fff0377d";
+                            parent[1].push("बचत");
                             break;
 
                         default:
@@ -140,8 +276,51 @@ function displayData() {
                     }
 
                 });
+                yAxis[0] = Object.values(counters.debit);
+                xAxis[0] = Object.keys(counters.debit);
+                yAxis[1] = Object.values(counters.Savings);
+                xAxis[1] = Object.keys(counters.Savings);
+                yAxis[2] = Object.values(counters.credit);
+                xAxis[2] = Object.keys(counters.credit);
+                for (var key in counters) {
 
+                    var subObj = counters[key];
+                    for (var subKey in subObj) {
+                        var SumRow = document.createElement("tr");
+                        var cell1 = document.createElement("td");
+                        cell1.innerHTML = subKey;
 
+                        var cell2 = document.createElement("td");
+                        cell2.innerHTML = subObj[subKey];
+                        SumRow.appendChild(cell1)
+                    
+                    switch (key) {
+                        case "credit":
+
+                            cell2.style.backgroundColor = "#12b1127d";
+                            
+                            SumRow.appendChild(cell2)
+                            break;
+                        case "debit":
+
+                            cell2.style.backgroundColor = "#b119127d";
+                            SumRow.appendChild(cell2)
+
+                            break;
+                        case "Savings":
+
+                            cell2.style.backgroundColor = "#fff0377d";
+                            SumRow.appendChild(cell2)
+                            break;
+
+                        default:
+                            cell2.innerHTML = "";
+                            break;
+                    }
+                    cate.appendChild(SumRow);
+
+                }}
+                Ploy(xAxis, yAxis, parent);
                 var IncomeRow = document.createElement("tr");
 
                 // Create and append cells for each column
